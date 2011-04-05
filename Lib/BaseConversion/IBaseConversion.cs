b0f7f0619a236a0c04ba704490c0211e9e2fef24
Marcio Toshio Ide <contato@marciotoshio.com.br>
2011-04-05 01:22:06 -0300

    Adding a interface for base conversion

---
+++ Lib/BaseConversion/IBaseConversion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPersonalShortner.Lib.BaseConversion
+{
+    public interface IBaseConversion
+    {
+        IList<int> Encode(int value);
+        int Decode(int[] values);
+    }
+}
