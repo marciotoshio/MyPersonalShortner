d5d0adc022171c37403d1565839bed9a1ba0122f
Marcio Toshio Ide <contato@marciotoshio.com.br>
2011-04-06 03:23:05 -0300

    Asp.Net MVC3 & Html5Boilerplate

---
+++ MvcApp/Controllers/ShortnerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPersonalShortner.MvcApp.Controllers
+{
+    public class ShortnerController : Controller
+    {
+        //
+        // GET: /Shortner/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
