a8c65c5f60d38f7abc312dc77a696e3e8b656a12
Marcio Toshio Ide <contato@marciotoshio.com.br>
2011-04-09 04:54:07 -0300

    removed hard-coded domain

--- MvcApp/Areas/Api/DTO/ApiDataResult.cs
+++ MvcApp/Areas/Api/DTO/ApiDataResult.cs
@@ -1,13 +1,13 @@
 ﻿using System.Runtime.Serialization;
+using MyPersonalShortner.MvcApp.Helpers;
 
 namespace MyPersonalShortner.MvcApp.Areas.Api.DTO
 {
     [DataContract(Name = "data")]
     public class ApiShortenResult : ApiResult
     {
-        // TODO: remove hard-coded domain
         [DataMember(Name = "url")]
-        public string Url { get { return "http://tosh.io/" + Hash; } }
+        public string Url { get { return AppHelper.GetFullHostAddress() + Hash; } }
 
         [DataMember(Name = "hash")]
         public string Hash { get; set; }
---
+++ MvcApp/Helpers/AppHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyPersonalShortner.MvcApp.Helpers
+{
+    public class AppHelper
+    {
+        public static string GetFullHostAddress()
+        {
+            return HttpContext.Current.Request.Url.Scheme + "://" + HttpContext.Current.Request.Url.Authority + "/";
+        }
+    }
+}
